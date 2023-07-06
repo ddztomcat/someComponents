99d3a31472934d1d76651bfbba5596ada353c9dc
ddz <ddztomcat@gmail.com>
2023-07-06 09:57:56 +0800

    初始化

---
+++ src/components/NumCountTo.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="amount">
+    <div style="display: flex">
+      <span>￥</span>
+      <transition-group name="list" tag="p" style="position: relative">
+        <div
+          style="display: inline-block; position: absolute"
+          v-for="(item, index) in todayAmountComputed"
+          :style="{
+            left: index * 53 + 'px',
+            'transition-delay': 0.1 * index + 's'
+          }"
+          :key="item + index"
+        >
+          {{ item }}
+        </div>
+      </transition-group>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive, computed, onMounted } from 'vue'
+const data = reactive({
+  todayAmount: '0',
+  platformSales1: 4090013
+})
+
+const todayAmountComputed = computed(() => {
+  return data.todayAmount.split('')
+})
+
+onMounted(() => {
+  //   setInterval(() => {
+  //     if (data.todayAmount === '0') {
+  //       data.todayAmount = '1000'
+  //     }
+  //     data.todayAmount = '' + Math.floor(Math.random() * 10000)
+  //   }, 2000)
+})
+</script>
+<style>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
+body,
+html {
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  position: relative;
+}
+
+.amount {
+  color: gold;
+  font-size: 85px;
+  display: flex;
+  justify-content: center;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: transform 1s ease, opacity 1s ease;
+}
+
+.list-enter-from {
+  opacity: 0;
+  transform: translateY(50%);
+}
+
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(-50%);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Num from '../components/NumAnimate.vue'
+import NumCount from '../components/NumCountTo.vue'
+const ns = ref([0, 0, 0, 0, 0])
+// setInterval(() => {
+//   ns.value.forEach((v, i) => {
+//     ns.value[i] = Math.ceil(Math.random() * 100) % 10
+//   })
+//   console.log(ns.value)
+// }, 3000)
+</script>
+
+<template>
+  <main>
+    <div class="flex">
+      <Num v-for="(n, i) in ns" :key="i" :number="n"></Num>
+    </div>
+    <NumCount></NumCount>
+  </main>
+</template>
+<style>
+.flex {
+  display: flex;
+}
+</style>
